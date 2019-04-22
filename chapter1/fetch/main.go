fa5f70575c5cc0a9b09940a9774392a003be3ac0
x899 <p.chheda29@gmail.com>
2019-04-22 18:12:48 +0530

    fetch url

---
+++ chapter1/fetch/main.go
@@ -0,0 +1,26 @@
+// Fetch prints the content found at a URL.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			continue
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			continue
+		}
+		fmt.Printf("%s\n", body)
+	}
+}
